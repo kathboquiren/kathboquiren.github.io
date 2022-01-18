2c9d02fbf855f7cfe37b2dfc8afdac9adb88e604
bgcdev <bgcdev@outlook.com>
2022-01-18 19:02:13 +0800

    React Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const url = 'https://randomuser.me/api/'
+  const defaultImage = 'https://randomuser.me/api/portraits/women/9.jpg'
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [randomPerson, setRandomPerson] = useState(null)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const refreshRandom = async () => {
+    setIsLoading(true)
+    const response = await fetch(url)
+    const data = await response.json()
+   
+    const person = data.results[0]
+    // destructure values
+    const {
+      email,
+      name: { first, last },
+      picture: { large: image }
+    } = person
+   
+    const newPerson = {
+      name: `${first} ${last}`,
+      image,
+      email
+    }
+
+    setRandomPerson(newPerson)
+    setIsLoading(false)
+    setName(newPerson.name)
+    setEmail(newPerson.email)
+  }
+
+  useEffect(() => {
+    refreshRandom()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className='container'>
+          <img
+            src={(randomPerson && randomPerson.image) || defaultImage}
+            alt='random user'
+            className='user-img'
+          />
+          <div className='user-value'>
+              <span className='userlabel'>Name: </span>
+              <span className='userValue'>{name}</span>
+              </div>
+          <div className='user-value'>
+              <span className='userlabel'>Email: </span>
+              <span className='userValue'>{email}</span>
+          </div>
+          <button className='btn' type='button' onClick={refreshRandom}>
+            {isLoading ? 'loading...' : 'Refresh'}
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
